a5d140e16f353e2d4b908196de6274f02e0539d2
pihani patel <pihanipatel982005@gmail.com>
2026-07-12 16:03:51 +0530

    changes to be commited

--- logic_box.py
+++ logic_box.py
@@ -1,39 +1,4 @@
-# print("Welcom to the Pattern Generator and Number Analyzer!")
 
-
-# print("Select an option:\n1. Generate a Pattern\n2. Analyze a Range of Numbers\n3. Exit")
-# num = int(input("Enter your choice: "))
-
-# rows = int(input("Enter the number of rows for the pattern: "))
-# print("\nPattern: \n")
-# for i in range (0,rows):
-#     for j in range (i+1):
-#         if rows <= 0:
-#            break
-#         if rows <= 0:
-#            pass 
-#         print("*",end=" ")
-#     print()    
-
-
-# print("Select an option:\n1. Generate a Pattern\n2. Analyze a Range of Numbers\n3. Exit")
-# num = int(input("Enter your choice: "))
-# start = int(input("Enter the start of the range: "))
-# end = int(input("Enter the end of the range: "))
-# for i in range (start,end):
-#     if i % 2 == 0:
-#         print("Number", i ,"is Even")
-#     else:
-#         print("Number", i ,"is Odd")
-#     sum += i
-# print("Sum of all numbers from", start, "to", end, "is: ",sum)    
-
-
-# print("Select an option:\n1. Generate a Pattern\n2. Analyze a Range of Numbers\n3. Exit")
-# num = int(input("Enter your choice: "))
-# print("Exiting the program. Goodbye!")
-
-######
 print("Welcom to the Pattern Generator and Number Analyzer!\n")
 
 for i in range (1,4):
